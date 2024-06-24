e81e21ec78a99bce367267ea3555a630c3e834de
anshjain16 <anshjain0216@gmail.com>
2024-06-24 17:07:13 +0530

    removing css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Register from './Auth/Register';
-import Login from './Auth/Login';
-import CompanyRegister from './Auth/CompanyRegister';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Register from "./Auth/Register";
+import Login from "./Auth/Login";
+import CompanyRegister from "./Auth/CompanyRegister";
+import AddHiringProcess from "./pages/company/AddHiringProcess";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/companyregister' element={<CompanyRegister />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/companyregister" element={<CompanyRegister />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/company/add-hiring-process"
+          element={<AddHiringProcess />}
+        />
       </Routes>
     </Router>
   );
