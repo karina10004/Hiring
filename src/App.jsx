abaffe7852f6d11c7db8a5b7b71d8a70b0bb5a47
karina <gs2110004@sgsitsindore.in>
2024-06-24 22:59:53 +0530

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/company/Dashboard';
+import Dashboard from './pages/company/companydashboard/CompanyHome';
 import Register from './Auth/Register';
 import Login from './Auth/Login';
 
