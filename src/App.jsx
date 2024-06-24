6bac82af2205bca4dee85316fb8c7c3bafb9f0e1
karina <gs2110004@sgsitsindore.in>
2024-06-24 23:18:33 +0530

    conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/company/Dashboard";
+import Dashboard from "./pages/company/companydashboard/CompanyHome";
 import Register from "./Auth/Register";
 import Login from "./Auth/Login";
 import CandidateHome from "./pages/candidate/Home";
-
+import CompanyRegister from './Auth/CompanyRegister'
+import CompanyLogin from './Auth/CompanyLogin'
 const App = () => {
   return (
     <Router>
@@ -13,6 +14,8 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/candidate" element={<CandidateHome />} />
+        <Route path="/companylogin" element={<CompanyLogin />} />
+        <Route path="/companyregister" element={<CompanyRegister />} />
       </Routes>
     </Router>
   );
