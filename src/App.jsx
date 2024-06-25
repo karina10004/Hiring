15b95292b38252a2d248ca9fa80abf8f07e94af0
anshjain16 <anshjain0216@gmail.com>
2024-06-25 17:48:43 +0530

    feat: manage hiring process and manage coding round

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import CompanyLogin from "./Auth/CompanyLogin";
 import AddHiring from "./pages/company/AddHiringProcess";
 import AddEmployee from "./pages/company/AddEmployee";
 import ManageHiringProcess from "./pages/company/ManageHiringProcess";
+import ManageCodingRound from "./pages/company/ManageCodingRound";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Route path="/hiring" element={<AddHiring />} />
         <Route path="/employee" element={<AddEmployee />} />
         <Route path="/manage/:id" element={<ManageHiringProcess />} />
+        <Route path="/coding/:id" element={<ManageCodingRound />} />
       </Routes>
     </Router>
   );
